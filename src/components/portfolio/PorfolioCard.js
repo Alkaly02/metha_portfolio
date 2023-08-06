9db35959d472ae096542fec1bc3aa78f91e6d47c
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2023-08-06 22:44:04 +0000

    faire la partie porfolio

---
+++ src/components/portfolio/PorfolioCard.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { Card, Image } from '@mantine/core';
+import { rootstyles } from '../../styles/rootstyles';
+import { styled } from 'styled-components';
+
+const CartTitle = styled.p`
+color: rgba(26, 16, 3, 0.50);
+font-family: Poppins;
+font-size: 10px;
+font-style: normal;
+font-weight: 400;
+line-height: 16px; /* 160% */
+margin:0
+`
+
+const CardDesc = styled.p`
+color: #1A1003;
+font-family: Poppins;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 16px; /* 100% */
+margin:0
+`
+
+const PorfolioCard = ({ title, description }) => {
+    return (
+        <Card bg={rootstyles.color.tercary} shadow="sm" padding="lg" radius="md" >
+            <Card.Section p='sm'>
+                <Image
+                    src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    height={160}
+                    alt="Norway" radius='md'
+                />
+            </Card.Section>
+
+            {/* <Group position="" mt={0} mb="xs"> */}
+            <CartTitle weight={100} sx={{ margin: 0 }}>{title}</CartTitle>
+            <CardDesc>{description}</CardDesc>
+            {/* <Badge color="pink" variant="light">
+                    On Sale
+                </Badge> */}
+            {/* </Group> */}
+        </Card>
+    )
+}
+
+export default PorfolioCard
--- src/pages/travail/TravailHomePage.jsx
+++ src/pages/travail/TravailHomePage.jsx
@@ -1,11 +1,46 @@
 import React from "react";
 import Title from "../../components/shared/Title";
 import { rootstyles } from "../../styles/rootstyles";
+import { Grid } from "@mantine/core";
+import PorfolioCard from "../../components/portfolio/PorfolioCard";
+
+const portfolio = {
+  all: [
+    {
+      title: "Développement d'applications",
+      description: "Smart Bank App",
+    },
+    {
+      title: "Blog",
+      description: "NEXT.js",
+    },
+    {
+      title: "UI/UX",
+      description: "Rental",
+    },
+    {
+      title: "Mentorat",
+      description: "Tech Mentor",
+    },
+    {
+      title: "Développement web",
+      description: "IdeaFlow",
+    },
+  ],
+};
 
 const TravailHomePage = () => {
   return (
     <div>
       <Title text="Portfolio" lineBg={rootstyles.color.navbarActive} />
+
+      <Grid>
+        {portfolio.all.map((work) => (
+          <Grid.Col span={6}>
+            <PorfolioCard title={work.title} description={work.description} />
+          </Grid.Col>
+        ))}
+      </Grid>
     </div>
   );
 };
